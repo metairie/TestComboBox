a9b9dddae787b5ed96c6addff489fa6f2f863617
metairie <metairie@ebu.ch>
2015-07-15 17:56:34 +0200

    try to find better way of doing list filtering

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,8 +12,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 import org.fxpart.KeyValueString;
@@ -23,7 +21,6 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
@@ -36,46 +33,72 @@ public class Controller implements Initializable {
     private static final KeyCodeCombination HOME = new KeyCodeCombination(KeyCode.HOME);
     private static final KeyCodeCombination TAB = new KeyCodeCombination(KeyCode.TAB);
     private static final KeyCodeCombination END = new KeyCodeCombination(KeyCode.END);
-    private static final String HIGHLIGHTED_DROPDOWN_CLASS = "highlighted-dropdown";
-    private static final String USUAL_DROPDOWN_CLASS = "usual-dropdown";
 
-    private ObservableList<KeyValueString> dataSource = FXCollections.observableArrayList(new MockDatas().loadLocation());
     private Function<String, List<KeyValueString>> searchFunction = (term -> {
-        ObservableList<KeyValueString> list = FXCollections.observableArrayList(getDataSource().stream().filter(new Predicate<KeyValueString>() {
-            @Override
-            public boolean test(KeyValueString item) {
-                return item.getValue().contains(term == null ? "" : term);
-            }
-        }).collect(Collectors.toList()));
+        ObservableList<KeyValueString> list = FXCollections.observableArrayList(getDataSource().stream().filter(item -> item.getValue().contains(term == null ? "" : term)).collect(Collectors.toList()));
         return list;
     });
 
     @FXML
     ComboBox<KeyValueString> combo;
 
+    static String some;
+    static String typedText;
+    static StringBuilder sb = new StringBuilder();
+
+    public enum AutoCompleteMode {
+        STARTS_WITH, CONTAINING,;
+    }
+
+    ObservableList<KeyValueString> data;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setCustomCellFactory();
-        setTextFieldFormatter(item -> String.format("%s", item.getValue()));
+        setTextFieldFormatter(new Function<KeyValueString, String>() {
+            @Override
+            public String apply(KeyValueString item) {
+                System.out.println(" setTextFieldFormatter (item.getValue) = " + item.getValue());
+                String ret = String.format("%s", item.getValue());
+                return ret;
+            }
+        });
+        combo.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                combo.hide();
+            }
+        });
         combo.addEventHandler(KeyEvent.KEY_RELEASED, createKeyReleaseEventHandler());
+        combo.setItems(FXCollections.observableArrayList(new MockDatas().loadLocation()));
+        data = combo.getItems();
     }
 
     private EventHandler<KeyEvent> createKeyReleaseEventHandler() {
         return event -> {
+            System.out.println(" k = " + combo.getEditor().getText());
             if (DOWN.match(event)) {
-                if (!combo.isShowing()) {
-                    combo.show();
-                }
+                showCombo();
                 return;
             } else if (UP.match(event) || RIGHT.match(event) || LEFT.match(event) || HOME.match(event) || END.match(event) || TAB.match(event) || event.isControlDown()) {
                 return;
             } else {
+                if (DELETE.match(event) || BACK_SPACE.match(event)) {
+                    System.out.println(" EventHandler " + combo.getEditor().getText());
+                }
                 // search if possible
-                ObservableList<KeyValueString> list = combo.getItems();
-                list.setAll(getSearchFunction().apply(combo.getEditor().getText()));
-                combo.show();
+//                combo.getItems().setAll(getSearchFunction().apply(combo.getEditor().getText()));
+                String term = combo.getEditor().getText();
+                List<KeyValueString> list = new MockDatas().loadLocation();
+                ObservableList<KeyValueString> list2 = FXCollections.observableArrayList(list);
+                showCombo();
             }
         };
+    }
+
+    private void showCombo() {
+        if (!combo.isShowing()) {
+            combo.show();
+        }
     }
 
     private void setTextFieldFormatter(Function<KeyValueString, String> textFieldFormatter) {
@@ -96,51 +119,18 @@ public class Controller implements Initializable {
         combo.setCellFactory(new Callback<ListView<KeyValueString>, ListCell<KeyValueString>>() {
                                  @Override
                                  public ListCell<KeyValueString> call(ListView<KeyValueString> param) {
-                                     param.setPrefHeight(150);
+                                     param.setPrefHeight(200);
+                                     param.setPrefWidth(400);
                                      final ListCell<KeyValueString> cell = new ListCell<KeyValueString>() {
                                          @Override
                                          protected void updateItem(KeyValueString item, boolean empty) {
                                              super.updateItem(item, empty);
+                                             System.out.println(" update item " + (item == null ? " NULL " : " " + item.getValue()));
                                              if (item == null || empty) {
-                                                 setGraphic(null);
+                                                 // setGraphic(null);
+                                                 setText(null);
                                              } else {
-                                                 HBox styledText = new HBox();
-                                                 String keyString = item.getKey();
-                                                 String valueString = (item).getValue();
-                                                 String itemString = keyString + " - " + valueString;
-                                                 if (combo.getEditor().getText().length() != 0) {
-                                                     Integer searchStringPosition = valueString.indexOf(combo.getEditor().getText());
-
-                                                     // itemString contains searchString. It should be split and searchString should be highLighted
-                                                     if (searchStringPosition >= 0) {
-                                                         String beginString = valueString.substring(0, searchStringPosition);
-                                                         String highlightedString = valueString.substring(searchStringPosition, searchStringPosition + combo.getEditor().getText().length());
-                                                         String endString = valueString.substring(searchStringPosition + combo.getEditor().getText().length());
-
-                                                         Text separator = new Text(keyString + " - ");
-                                                         separator.getStyleClass().add(USUAL_DROPDOWN_CLASS);
-                                                         styledText.getChildren().add(separator);
-
-                                                         final Text begin = new Text(beginString);
-                                                         begin.getStyleClass().add(USUAL_DROPDOWN_CLASS);
-                                                         styledText.getChildren().add(begin);
-
-                                                         final Text highlighted = new Text(highlightedString);
-                                                         highlighted.getStyleClass().add(HIGHLIGHTED_DROPDOWN_CLASS);
-                                                         styledText.getChildren().add(highlighted);
-
-                                                         final Text end = new Text(endString);
-                                                         end.getStyleClass().add(USUAL_DROPDOWN_CLASS);
-                                                         styledText.getChildren().add(end);
-
-
-                                                     } else {
-                                                         styledText.getChildren().add(new Text(itemString));
-                                                     }
-                                                 } else {
-                                                     styledText.getChildren().add(new Text(itemString));
-                                                 }
-                                                 setGraphic(styledText);
+                                                 setText(item.getKey() + " - " + item.getValue());
                                              }
                                          }
                                      };
@@ -151,11 +141,7 @@ public class Controller implements Initializable {
     }
 
     public List<KeyValueString> getDataSource() {
-        return dataSource;
-    }
-
-    public void setDataSource(ObservableList<KeyValueString> dataSource) {
-        this.dataSource = dataSource;
+        return FXCollections.observableArrayList(new MockDatas().loadLocation());
     }
 
     public Function<String, List<KeyValueString>> getSearchFunction() {
@@ -163,6 +149,6 @@ public class Controller implements Initializable {
     }
 
     public void click(ActionEvent actionEvent) {
-
     }
+
 }
